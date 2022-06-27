57d5972017af2e7a60dad731095767511e3b8462
Fraida Fund <ffund@nyu.edu>
2022-06-27 12:25:19 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,1 +1,21 @@
+"""This is a basic profile with one node.
+Instructions:
+Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
+"""
 
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+ 
+# Add a raw PC to the request.
+node = request.RawPC("node")
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
