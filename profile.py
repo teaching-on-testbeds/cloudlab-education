147b91bd9731971dda64c2a4621106ff854642d5
Fraida Fund <ffund@nyu.edu>
2022-06-27 13:45:18 -0400

    Update profile for adaptive video exp

---
+++ profile.py
@@ -0,0 +1,55 @@
+"""Cloudlab profile for the experiment at https://witestlab.poly.edu/blog/adaptive-video-reproducing/ """
+
+#
+# NOTE: This code was machine converted. An actual human would not
+#       write code like this!
+#
+
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+# Import the Emulab specific extensions.
+import geni.rspec.emulab as emulab
+
+# Create a portal object,
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+# Node client
+node_client = request.XenVM('client')
+node_client.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
+node_client.Site('Site 1')
+iface0 = node_client.addInterface('interface-0', pg.IPv4Address('10.10.1.100','255.255.255.0'))
+
+# Node router
+node_router = request.XenVM('router')
+node_router.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
+node_router.Site('Site 1')
+iface1 = node_router.addInterface('interface-1', pg.IPv4Address('10.10.1.1','255.255.255.0'))
+iface2 = node_router.addInterface('interface-2', pg.IPv4Address('10.10.2.1','255.255.255.0'))
+
+# Node server
+node_server = request.XenVM('server')
+node_server.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
+node_server.Site('Site 1')
+iface3 = node_server.addInterface('interface-3', pg.IPv4Address('10.10.2.100','255.255.255.0'))
+
+# Link link-0
+link_0 = request.Link('link-0')
+link_0.Site('undefined')
+link_0.addInterface(iface0)
+link_0.addInterface(iface1)
+
+# Link link-1
+link_1 = request.Link('link-1')
+link_1.Site('undefined')
+link_1.addInterface(iface2)
+link_1.addInterface(iface3)
+
+
+# Print the generated rspec
+pc.printRequestRSpec(request)
+
