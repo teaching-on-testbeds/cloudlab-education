f6f79cd2f78a439dead70f652c935ac145fdaf98
Fraida Fund <ffund@nyu.edu>
2022-07-08 15:49:53 -0400

    Add TCP congestion control Rspec

--- profile.py
+++ profile.py
@@ -1,12 +1,12 @@
 """ 
 
-## Adaptive video
+## TCP congestion control
 
-This experiment explores the tradeoff between different metrics of video quality (average rate, interruptions, and variability of rate) in an adaptive video delivery system.
+This experiment shows the basic behavior of TCP congestion control. You'll see the classic "sawtooth" pattern in a TCP flow's congestion window, and you'll see how a TCP flow responds to congestion indicators.
 
-It should take about 60-120 minutes to run this experiment.
+It should take about 1 hour to run this experiment.
 
-Instructions for running the experiment are at: https://witestlab.poly.edu/blog/adaptive-video-reproducing/ """
+Instructions for running the experiment are at: https://witestlab.poly.edu/blog/tcp-congestion-control-basics/ """
 
 #
 # NOTE: This code was machine converted. An actual human would not
@@ -26,36 +26,39 @@ pc = portal.Context()
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
 
-# Node client
-node_client = request.XenVM('client')
-node_client.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
-node_client.Site('Site 1')
-iface0 = node_client.addInterface('interface-0', pg.IPv4Address('10.10.1.100','255.255.255.0'))
+# Node romeo
+node_romeo = request.XenVM('romeo')
+node_romeo.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
+node_romeo.Site('Site 1')
+node_romeo.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
+iface0 = node_romeo.addInterface('interface-1', pg.IPv4Address('10.10.1.100','255.255.255.0'))
+
+# Node juliet
+node_juliet = request.XenVM('juliet')
+node_juliet.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
+node_juliet.Site('Site 1')
+node_juliet.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
+iface1 = node_juliet.addInterface('interface-3', pg.IPv4Address('10.10.2.100','255.255.255.0'))
 
 # Node router
 node_router = request.XenVM('router')
-node_router.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
+node_router.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
 node_router.Site('Site 1')
-iface1 = node_router.addInterface('interface-1', pg.IPv4Address('10.10.1.1','255.255.255.0'))
-iface2 = node_router.addInterface('interface-2', pg.IPv4Address('10.10.2.1','255.255.255.0'))
-
-# Node server
-node_server = request.XenVM('server')
-node_server.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
-node_server.Site('Site 1')
-iface3 = node_server.addInterface('interface-3', pg.IPv4Address('10.10.2.100','255.255.255.0'))
+node_router.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
+iface2 = node_router.addInterface('interface-0', pg.IPv4Address('10.10.1.1','255.255.255.0'))
+iface3 = node_router.addInterface('interface-2', pg.IPv4Address('10.10.2.1','255.255.255.0'))
 
 # Link link-0
 link_0 = request.Link('link-0')
 link_0.Site('undefined')
+link_0.addInterface(iface2)
 link_0.addInterface(iface0)
-link_0.addInterface(iface1)
 
 # Link link-1
 link_1 = request.Link('link-1')
 link_1.Site('undefined')
-link_1.addInterface(iface2)
 link_1.addInterface(iface3)
+link_1.addInterface(iface1)
 
 
 # Print the generated rspec
