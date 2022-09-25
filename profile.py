da255d06de1cc3fe44c6c4c757b2fdfbcb74e6f0
Fraida Fund <ffund@nyu.edu>
2022-09-24 23:14:39 -0400

    Update profile to enable VNC

--- profile.py
+++ profile.py
@@ -29,34 +29,40 @@ request = pc.makeRequestRSpec()
 # Node romeo
 node_romeo = request.XenVM('romeo')
 node_romeo.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
-node_romeo.Site('Site 1')
+#node_romeo.Site('Site 1')
 node_romeo.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
+node_romeo.startVNC()
+node_romeo.routable_control_ip = True
 iface0 = node_romeo.addInterface('interface-1', pg.IPv4Address('10.10.1.100','255.255.255.0'))
 
 # Node juliet
 node_juliet = request.XenVM('juliet')
 node_juliet.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
-node_juliet.Site('Site 1')
+#node_juliet.Site('Site 1')
 node_juliet.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
+node_juliet.startVNC()
+node_juliet.routable_control_ip = True
 iface1 = node_juliet.addInterface('interface-3', pg.IPv4Address('10.10.2.100','255.255.255.0'))
 
 # Node router
 node_router = request.XenVM('router')
 node_router.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
-node_router.Site('Site 1')
+#node_router.Site('Site 1')
 node_router.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
+node_router.startVNC()
+node_router.routable_control_ip = True
 iface2 = node_router.addInterface('interface-0', pg.IPv4Address('10.10.1.1','255.255.255.0'))
 iface3 = node_router.addInterface('interface-2', pg.IPv4Address('10.10.2.1','255.255.255.0'))
 
 # Link link-0
 link_0 = request.Link('link-0')
-link_0.Site('undefined')
+#link_0.Site('undefined')
 link_0.addInterface(iface2)
 link_0.addInterface(iface0)
 
 # Link link-1
 link_1 = request.Link('link-1')
-link_1.Site('undefined')
+#link_1.Site('undefined')
 link_1.addInterface(iface3)
 link_1.addInterface(iface1)
 
