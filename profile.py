3cdbffeef5d4e45a4f9f7605f2641ba310829432
Fraida Fund <ffund@nyu.edu>
2023-03-14 21:31:43 -0400

    Add dijkstra profile

--- profile.py
+++ profile.py
@@ -1,5 +1,5 @@
 """ 
-## Adaptive video
+## Dijkstra's algorithm
 In this experiment, we will use Dijkstra's algorithm to find the shortest path from one node in a six-node topology, to all other nodes. We will then install routing rules at each node to implement the shortest-path tree produced by Dijkstra's algorithm.
 It should take about 120 minutes to run this experiment.
 Instructions for running the experiment are at: https://witestlab.poly.edu/blog/dijkstras-shortest-path-algorithm/ """
