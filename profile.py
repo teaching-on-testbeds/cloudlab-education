ad361350f588962309e7616782633e5cc28e35aa
Fraida Fund <ffund@nyu.edu>
2023-09-06 12:59:00 -0400

    Add a line profile with ubuntu 22, and addresses compatible with Chameleon

--- profile.py
+++ profile.py
@@ -19,22 +19,31 @@ request = pc.makeRequestRSpec()
 
 # Node romeo
 node_romeo = request.XenVM('romeo')
-node_romeo.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
+node_romeo.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_romeo.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
 iface0 = node_romeo.addInterface('interface-1', pg.IPv4Address('10.10.1.100','255.255.255.0'))
+node_romeo.exclusive = False
+node_romeo.routable_control_ip = True # required for VNC
+node_romeo.startVNC()
 
 # Node juliet
 node_juliet = request.XenVM('juliet')
-node_juliet.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
+node_juliet.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_juliet.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
 iface1 = node_juliet.addInterface('interface-3', pg.IPv4Address('10.10.2.100','255.255.255.0'))
+node_juliet.exclusive = False
+node_juliet.routable_control_ip = True # required for VNC
+node_juliet.startVNC()
 
 # Node router
 node_router = request.XenVM('router')
-node_router.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
+node_router.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_router.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
-iface2 = node_router.addInterface('interface-0', pg.IPv4Address('10.10.1.1','255.255.255.0'))
-iface3 = node_router.addInterface('interface-2', pg.IPv4Address('10.10.2.1','255.255.255.0'))
+iface2 = node_router.addInterface('interface-0', pg.IPv4Address('10.10.1.10','255.255.255.0'))
+iface3 = node_router.addInterface('interface-2', pg.IPv4Address('10.10.2.10','255.255.255.0'))
+node_router.exclusive = False
+node_router.routable_control_ip = True # required for VNC
+node_router.startVNC()
 
 # Link link-0
 link_0 = request.Link('link-0')
