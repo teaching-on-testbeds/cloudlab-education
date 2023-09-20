5a240227a06503d3aedd6a83d11b2dd137fe51f7
Fraida Fund <ffund@nyu.edu>
2023-09-20 15:22:29 -0400

    Add instructions to profile

--- profile.py
+++ profile.py
@@ -5,7 +5,7 @@
 
 The exercises in this experiment will focus on security services offered at the **network layer** of the TCP/IP protocol stack. You will configure a network with a VPN tunnel, and then you will examine the extent to which you are protected from unauthorized eavesdroppers on network traffic, when you use a file transfer application (with and without [application layer confidentiality](https://witestlab.poly.edu/blog/secure-networked-applications/)).
 
-It should take about 60-120 minutes to run this experiment.
+It should take about 60 minutes to run this experiment.
 
 Instructions for running the experiment are at: https://witestlab.poly.edu/blog/network-layer-security """
 
