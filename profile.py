8b1d5de748b47994134e7096d3748e6ff91f1ac3
Fraida Fund <ffund@nyu.edu>
2023-10-13 13:39:36 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -20,7 +20,7 @@ request = pc.makeRequestRSpec()
 # Node romeo
 node_romeo = request.XenVM('romeo')
 node_romeo.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
-node_romeo.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
+node_romeo.addService(pg.Execute('/bin/sh','wget -O - https://raw.githubusercontent.com/ffund/tcp-ip-essentials/cloudlab/scripts/no-offload.sh | bash'))
 iface0 = node_romeo.addInterface('interface-1', pg.IPv4Address('10.0.1.100','255.255.255.0'))
 node_romeo.exclusive = False
 node_romeo.routable_control_ip = True # required for VNC
@@ -29,7 +29,7 @@ node_romeo.startVNC()
 # Node juliet
 node_juliet = request.XenVM('juliet')
 node_juliet.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
-node_juliet.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
+node_juliet.addService(pg.Execute('/bin/sh','wget -O - https://raw.githubusercontent.com/ffund/tcp-ip-essentials/cloudlab/scripts/no-offload.sh | bash'))
 iface1 = node_juliet.addInterface('interface-3', pg.IPv4Address('10.0.2.100','255.255.255.0'))
 node_juliet.exclusive = False
 node_juliet.routable_control_ip = True # required for VNC
@@ -38,7 +38,7 @@ node_juliet.startVNC()
 # Node router
 node_router = request.XenVM('router')
 node_router.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
-node_router.addService(pg.Execute('/bin/sh','wget -O - https://git.io/JTQIx | bash'))
+node_router.addService(pg.Execute('/bin/sh','wget -O - https://raw.githubusercontent.com/ffund/tcp-ip-essentials/cloudlab/scripts/no-offload.sh | bash'))
 iface2 = node_router.addInterface('interface-0', pg.IPv4Address('10.0.1.10','255.255.255.0'))
 iface3 = node_router.addInterface('interface-2', pg.IPv4Address('10.0.2.10','255.255.255.0'))
 node_router.exclusive = False
