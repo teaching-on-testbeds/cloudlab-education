321de79c9e24274d75d993acb6827bf2ab786255
Fraida Fund <ffund@nyu.edu>
2024-09-20 15:17:51 -0400

    Update profile

--- profile.py
+++ profile.py
@@ -5,14 +5,14 @@ To run this experiment, follow the instructions at: https://witestlab.poly.edu/b
 
 # Import the Portal object.
 import geni.portal as portal
+# Emulab specific extensions.
+import geni.rspec.emulab as emulab
 # Import the ProtoGENI library.
-import geni.rspec.pg as pg
+import geni.rspec.pg as rspec
 
-# Create a portal context.
-pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
-request = pc.makeRequestRSpec()
+request = portal.context.makeRequestRSpec()
 
 # Declare that you will be starting X11 VNC on (some of) your nodes.
 # You must have this line for X11 VNC to work.
@@ -21,7 +21,7 @@ request.initVNC()
 
 node_alice = request.XenVM('alice')
 node_alice.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
-node_alice.addService(pg.Execute(shell="bash", command="/usr/bin/sudo /usr/bin/apt purge firefox; /usr/bin/sudo /usr/bin/snap remove firefox; /usr/bin/sudo /usr/bin/add-apt-repository ppa:mozillateam/ppa -y ; /usr/bin/sudo /usr/bin/apt -y install firefox-esr; /usr/bin/sudo /usr/bin/ln -s /usr/bin/firefox-esr /usr/local/bin/firefox"))
+node_alice.addService(rspec.Execute(shell="bash", command="/usr/bin/sudo /usr/bin/apt purge firefox; /usr/bin/sudo /usr/bin/snap remove firefox; /usr/bin/sudo /usr/bin/add-apt-repository ppa:mozillateam/ppa -y ; /usr/bin/sudo /usr/bin/apt -y install firefox-esr; /usr/bin/sudo /usr/bin/ln -s /usr/bin/firefox-esr /usr/local/bin/firefox"))
 node_alice.routable_control_ip = True # required for VNC
 node_alice.startVNC()
 
