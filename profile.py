d9d2c40dabbee0795c0f680a3a7888f9e9939d43
Fraida Fund <ffund@nyu.edu>
2024-09-20 15:20:33 -0400

    Update profile

--- profile.py
+++ profile.py
@@ -5,31 +5,32 @@ To run this experiment, follow the instructions at: https://witestlab.poly.edu/b
 
 # Import the Portal object.
 import geni.portal as portal
-# Emulab specific extensions.
-import geni.rspec.emulab as emulab
 # Import the ProtoGENI library.
 import geni.rspec.pg as rspec
+# Emulab specific extensions.
+import geni.rspec.emulab as emulab
 
+# Create a portal context, needed to defined parameters
+pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
-request = portal.context.makeRequestRSpec()
+request = pc.makeRequestRSpec()
 
-# Declare that you will be starting X11 VNC on (some of) your nodes.
-# You must have this line for X11 VNC to work.
-request.initVNC()
-
-
+# Set up first host - alice
 node_alice = request.XenVM('alice')
-node_alice.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
+node_alice.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
 node_alice.addService(rspec.Execute(shell="bash", command="/usr/bin/sudo /usr/bin/apt purge firefox; /usr/bin/sudo /usr/bin/snap remove firefox; /usr/bin/sudo /usr/bin/add-apt-repository ppa:mozillateam/ppa -y ; /usr/bin/sudo /usr/bin/apt -y install firefox-esr; /usr/bin/sudo /usr/bin/ln -s /usr/bin/firefox-esr /usr/local/bin/firefox"))
 node_alice.routable_control_ip = True # required for VNC
 node_alice.startVNC()
 
 node_ca = request.XenVM('ca')
+node_ca.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
 
 node_website = request.XenVM('website')
+node_website.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
 
-node_ca = request.XenVM('mallory')
+node_mallory = request.XenVM('mallory')
+node_mallory.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
